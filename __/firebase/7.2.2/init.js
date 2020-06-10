dc42e33e7f800a7f19abe18d4b641c323533741f
francesco romeo <f.romeo.f@gmail.com>
2020-06-10 15:59:19 +0200

    first commit

---
+++ __/firebase/7.2.2/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCTfm7OPuTgpRjjEHO0L3jyUv4NVzcphp0",
+  "appId": "1:801967287412:web:13b4da69b28bbc3fa83521",
+  "authDomain": "theoptimumcompany-72922.firebaseapp.com",
+  "databaseURL": "https://theoptimumcompany-72922.firebaseio.com",
+  "measurementId": "G-QSYY3E6E8P",
+  "messagingSenderId": "801967287412",
+  "projectId": "theoptimumcompany-72922",
+  "storageBucket": "theoptimumcompany-72922.appspot.com"
+});
